51e6865a8ba36abd11fea7a27ffb49c3e22ab58b
Akzam Khamitov <makoron3002@gmail.com>
2023-08-22 21:00:36 +0600

    Initial commit

---
+++ src/main/java/com/cycnet/ctfPlatform/CtfPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cycnet.ctfPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cycnet/ctfPlatform/CtfPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cycnet.ctfPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
